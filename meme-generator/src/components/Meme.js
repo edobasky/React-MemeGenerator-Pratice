beccd3bc994be873df06c8592a74bb7dab40fd1e
edobasky <edore7@gmail.com>
2022-04-12 03:55:06 +0100

    updated memegenerator/added input elements

--- meme-generator/src/components/Meme.js
+++ meme-generator/src/components/Meme.js
@@ -5,7 +5,7 @@ export default function Meme() {
 
    // const [memeImage, setImage] = React.useState("https://i.imgflip.com/30b1gx.jpg");
     const [meme, setImage] = React.useState({
-        topTex : "",
+        topText : "",
         bottomText : "",
         randomImage : "https://i.imgflip.com/30b1gx.jpg"
     });
@@ -23,6 +23,14 @@ export default function Meme() {
        }))
     }
 
+    function handleChange(event) {
+        const {name,value} = event.target
+        setImage(prevImage => ({
+            ...prevImage,
+            [name] : value
+        }))
+    }
+
     
 
 
@@ -33,10 +41,17 @@ export default function Meme() {
                  type="text"
                  placeholder="Top-text"
                  className="form--input"
+                 name="topText"
+                 value={meme.topText}
+                 onChange={handleChange}
+
                  />
                 <input type="text"
                 placeholder="Bottom--text"
                 className="form--input"
+                name="bottomText"
+                value={meme.bottomText}
+                onChange={handleChange}
                 />
                 <button
                 className="form--button" 
@@ -46,7 +61,12 @@ export default function Meme() {
                 </button>
             </div>
            
+           <div className="meme">
            <img src={meme.randomImage} alt="pics" className="meme--image"/>
+           <h2 className="meme--text top">{meme.topText}</h2>
+            <h2 className="meme--text bottom">{meme.bottomText}</h2>
+           </div>
+          
           
         </main>
     )
